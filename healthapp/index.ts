7c93dd118a5ea17b9cc5572710e6d4e07d8f3e74
vdedov <dedov.tech@google.com>
2026-08-11 21:20:26 +0200

    5. WebBmi

--- healthapp/index.ts
+++ healthapp/index.ts
@@ -1,8 +1,29 @@
 import express from 'express';
 const app = express();
+import { calculateBmi } from './bmiCalculator.ts'
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get("/bmi", (req, res) => {
+  const weight = Number(req.query.weight);
+  const height = Number(req.query.height);
+
+  if (isNaN(weight) || isNaN(height)) {
+    res.status(400).json({
+      error: "malformatted parameters",
+    });
+    return;
+  }
+
+  const result = calculateBmi(height, weight);
+
+  res.json({
+    weight,
+    height,
+    bmi: result,
+  });
 });
 
 const PORT = 3003;
